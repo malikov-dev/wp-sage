700292202c667fe4f5714bf9aef132106c0b8228
nail-php <malikov.kzn@gmail.com>
2022-09-02 15:24:11 +0300

    breadcrumbs

--- web/app/themes/sage/resources/views/components/breadcrumbs/wrap.blade.php
+++ web/app/themes/sage/resources/views/components/breadcrumbs/wrap.blade.php
@@ -1,3 +1,4 @@
 @if ( function_exists( 'yoast_breadcrumb' ) )
+  {{--  настроить можно в Yoast SEO->Отображение в поисковой выдаче->Цепочки навигации  --}}
   @php( yoast_breadcrumb( '<div class="breadcrumbs">', '</div>' ) )
 @endif
